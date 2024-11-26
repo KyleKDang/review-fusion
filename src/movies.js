20eaec423e36340ea8b6ef2456a0e4b8ea92ad38
Kyle Dang <kyledang@Kyles-MacBook-Air.local>
2024-11-25 21:17:28 -0800

    Add more sample movies

--- src/movies.js
+++ src/movies.js
@@ -38,7 +38,47 @@ const movies = [
       rating: 9.2,
       releaseDate: "March 24, 1972",
       poster: "/images/the-godfather.jpg",
-    }
+    },
+    {
+      id: 6,
+      title: "Schindler's List",
+      genre: "War/Drama",
+      rating: 9,
+      releaseDate: "February 4, 1994",
+      poster: "/images/schindlers-list.jpg"
+    },
+    {
+      id: 7,
+      title: "The Godfather Part II",
+      genre: "Crime/Drama",
+      rating: 9,
+      releaseDate: "December 20, 1974",
+      poster: "/images/the-godfather-part-2.jpg"
+    },
+    {
+      id: 8,
+      title: "Pulp Fiction",
+      genre: "Crime/Thriller",
+      rating: 8.9,
+      releaseDate: "October 14, 1994",
+      poster: "/images/pulp-fiction.jpg"
+    },
+    {
+      id: 9,
+      title: "The Lord of the Rings: The Return of the King",
+      genre: "Fantasy/Adventure",
+      rating: 9,
+      releaseDate: "December 17, 2003",
+      poster: "/images/lotr-the-return-of-the-king.jpg"
+    },
+    {
+      id: 10,
+      title: "Interstellar",
+      genre: "Sci-Fi/Adventure",
+      rating: 8.7,
+      releaseDate: "October 26, 2014",
+      poster: "/images/interstellar.jpg"
+    },
 ];
 
 export default movies;
