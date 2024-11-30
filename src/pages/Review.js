eed2f39215d1eeb2bf4a5e6acbe7008e195c9a45
Kyle Dang <kyledang@Kyles-MacBook-Air.local>
2024-11-29 17:47:24 -0800

    Add message on reviews page if reviews list is empty

--- src/pages/Review.js
+++ src/pages/Review.js
@@ -4,6 +4,7 @@ import ReviewList from '../components/ReviewList';
 export default function Review({ reviews }) {
     return (
         <>
+        {reviews.length === 0 && <h1><em>No reviews have been submitted yet</em></h1>}
         <ReviewList reviews={reviews} />
         </>
     )
