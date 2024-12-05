8e23c3afb32ce1c6a717132081f2858bce18859f
Kyle Dang <kyledang@Kyles-MacBook-Air.local>
2024-12-05 14:58:40 -0800

    Removed sensitive data

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -8,7 +8,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDExkXRxkWFwT3wGerw62FiDI05mBqUzzc",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "review-fusion.firebaseapp.com",
   projectId: "review-fusion",
   storageBucket: "review-fusion.firebasestorage.app",
